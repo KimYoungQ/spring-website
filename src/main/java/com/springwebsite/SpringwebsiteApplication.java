82ff413faaf1c03ec2e91869ede8ec401fd91976
young gyu Kim <dudeb0206@daum.net>
2021-02-01 15:35:04 +0900

    first commit

---
+++ src/main/java/com/springwebsite/SpringwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.springwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwebsiteApplication.class, args);
+    }
+
+}
