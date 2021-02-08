b9c81ac1ac530927cdabd7c8baf864f044e8ae59
young gyu Kim <dudeb0206@daum.net>
2021-02-08 12:47:20 +0900

    static resource reload setting.

--- src/main/java/com/springwebsite/config/MvcConfig.java
+++ src/main/java/com/springwebsite/config/MvcConfig.java
@@ -5,6 +5,7 @@ import com.springwebsite.navMenu.NavMenuService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,5 +18,11 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new NavMenuInterceptor(navMenuService))
                 .addPathPatterns("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {     // 서버 재시작 없이 정적 리소스 자원 리로드.
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:src/main/resources/templates/", "file:src/main/resources/static/");
     }
 }
